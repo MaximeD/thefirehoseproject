381140e62978623b46938fadf243bd09516cf182
Maxime Demolin <akbarova.armia@gmail.com>
2016-03-06 16:55:42 +0100

    ADD quotes listing
    We want to display the list of quotes inserted inside database.
    
    Details
    * COMPLTE lesson 4
    * ADD `quotes` resources in routes
    * ADD action to `QuotesController#index`
    * ADD template for quotes

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -1,7 +1,15 @@
 defmodule Thefirehoseproject.QuoteController do
   use Thefirehoseproject.Web, :controller
 
+  # `GET /`
   def homepage(conn, _params) do
     render conn, "homepage.html"
+  end
+
+  # `GET /quotes`: renders list of quotes
+  def index(conn, _params) do
+    conn
+    |> assign(:quotes, Repo.all(Thefirehoseproject.Quote))
+    |> render("index.html")
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Thefirehoseproject.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", QuoteController, :homepage
+    resources "/quotes", QuoteController
   end
 
   # Other scopes may use custom stacks.
