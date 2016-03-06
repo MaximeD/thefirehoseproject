1a67e631f48d6c8b8a6c42366aa6fb4bf9784453
Maxime Demolin <akbarova.armia@gmail.com>
2016-03-06 17:02:06 +0100

    ADD creation of new quotes
    Now we can display the list of quotes, next step is to create new ones.
    
    Details
    * COMPLETE lesson 5
    * ADD action `QuoteController#new`
    * ADD action `QuoteController#create`
    * ADD template to create a new quote
    * ADD changeset to `Quote` model
    * UPDATE `QuoteController` with default syntax

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -1,5 +1,8 @@
 defmodule Thefirehoseproject.QuoteController do
   use Thefirehoseproject.Web, :controller
+  alias Thefirehoseproject.Quote
+
+  plug :scrub_params, "quote" when action in [:create, :update]
 
   # `GET /`
   def homepage(conn, _params) do
@@ -8,8 +11,27 @@ defmodule Thefirehoseproject.QuoteController do
 
   # `GET /quotes`: renders list of quotes
   def index(conn, _params) do
-    conn
-    |> assign(:quotes, Repo.all(Thefirehoseproject.Quote))
-    |> render("index.html")
+    quotes = Repo.all(Quote)
+    render(conn, "index.html", quotes: quotes)
+  end
+
+  # `GET /quotes/new`: renders form to create a new quote
+  def new(conn, _params) do
+    changeset = Quote.changeset(%Quote{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  # `POST /quotes`: creates a new quote
+  def create(conn, %{"quote" => quote_params}) do
+    changeset = Quote.changeset(%Quote{}, quote_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _quote} ->
+        conn
+        |> put_flash(:info, "Quote created successfully.")
+        |> redirect(to: quote_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
--- web/models/quote.ex
+++ web/models/quote.ex
@@ -7,4 +7,18 @@ defmodule Thefirehoseproject.Quote do
 
     timestamps
   end
+
+  @required_fields ~w(saying author)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
 end
